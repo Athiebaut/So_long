2b9b3e368f4069374400d416d0d5cd65da018cd2
alix <alix@Debian.myguest.virtualbox.org>
2023-07-28 19:07:25 +0200

    28/07

--- Src/remplir_fenetre.c
+++ Src/remplir_fenetre.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   remplir_fenetre.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: athiebau <athiebau@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alix <alix@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 12:50:17 by athiebau          #+#    #+#             */
-/*   Updated: 2023/07/28 15:29:52 by athiebau         ###   ########.fr       */
+/*   Updated: 2023/07/28 18:51:57 by alix             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 void	mettre_limites(t_jeu *so_long)
 {
-	so_long->fichier = mlx_xpm_file_to_image(so_long->mlx,
-				"./Img/Mur.xpm", &so_long->pixel, &so_long->pixel);
-			mlx_put_image_to_window(so_long->mlx, so_long->mlx_win, so_long->fichier,
-				so_long->pixel * so_long->x, so_long->pixel * so_long->y);
+	so_long->fichier = mlx_xpm_file_to_image(so_long->mlx, "./Img/Mur.xpm",
+				&so_long->pixel, &so_long->pixel);
+	mlx_put_image_to_window(so_long->mlx, so_long->mlx_win, so_long->fichier,
+		so_long->pixel * so_long->x, so_long->pixel * so_long->y);
 }
 
 void	mettre_joueur(t_jeu *so_long)
 {
-	so_long->fichier = mlx_xpm_file_to_image(so_long->mlx,
-				"./Img/perso.xpm", &so_long->pixel, &so_long->pixel);
-			mlx_put_image_to_window(so_long->mlx, so_long->mlx_win, so_long->fichier,
-				50 * so_long->x, 50 * so_long->y);
+	so_long->fichier = mlx_xpm_file_to_image(so_long->mlx, "./Img/perso1.xpm",
+				&so_long->pixel, &so_long->pixel);
+	mlx_put_image_to_window(so_long->mlx, so_long->mlx_win, so_long->fichier,
+		so_long->pixel * so_long->x, so_long->pixel * so_long->y);
 }
